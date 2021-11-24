b7d370a97356d4745cb90539c98ce67a56828728
Kevin Quito <54966470+shincodeafk@users.noreply.github.com>
2021-11-23 23:44:02 -0500

    changed config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // it's good practice to put the sensitive data in their own file such as config.js
 // SECRET_KEY:  doesn't matter what's in here since it will be used as the key to decode the token
 module.exports = {
-    MONGODB: 'mongodb+srv://KevinQuito:chipmunk1@cluster0.laogv.mongodb.net/merng?retryWrites=true&w=majority',
+    MONGODB: 'mongodb+srv://KevinQuito:2Y4RQcN4nLogAUxR@cluster0.laogv.mongodb.net/merng?retryWrites=true&w=majority',
     SECRET_KEY: 'some very secret key'
 
 }
